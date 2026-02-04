e22b349a45c196cd590e4529d5bca4f6ef8f43b9
george li <george.li98@gmail.com>
2026-02-03 22:22:34 -0500

    remove config

--- src/config.js
+++
@@ -1,4 +0,0 @@
-// Configuration file - set environment variables
-// In production, you might want to load these from a server endpoint or build process
-window.VITE_SUPABASE_URL = 'https://mmwruhcvgyoazzvtonsd.supabase.co'
-window.VITE_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1td3J1aGN2Z3lvYXp6dnRvbnNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYyNjk5MDQsImV4cCI6MjA4MTg0NTkwNH0.y41ZXd6DkUqQ7kiL_Ml7f3SKguJVuCKlg0N_iU2lAwA'
